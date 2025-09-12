3363514e8a56a9467938faa10aab6e739b4b6b86
mariaradelinskidev <mariaradelinski.ti@gmail.com>
2025-09-12 11:47:26 -0300

    Alteração da música

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Olá professor, adorei a matéria"}
-
+    return {"Island in the stream também é muito boa"
+            "Baby, when I met you, there was peace unknown "
+            "I set out to get you with a fine-tooth comb"
+            "I was soft inside"
+            "There was something going on"}
